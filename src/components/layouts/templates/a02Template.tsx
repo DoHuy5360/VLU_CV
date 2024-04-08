4299c4aa7beb794ff24e7120177ecacece823a09
DoHuyGists <loldh321@gmail.com>
2024-04-08 23:46:53 +0700

    Update: ...

--- src/components/layouts/templates/a02Template.tsx
+++ src/components/layouts/templates/a02Template.tsx
@@ -18,30 +18,28 @@ import { UserData } from "@/types/userData";
 
 function A02Template(data: UserData) {
 	return (
-		<div className='w-full p-2 bg-slate-200'>
-			<div className='m-auto sm:w-11/12 md:w-2/3 lg:w-2/3 xl:w-2/4  p-4 bg-white'>
-				<div id='sectionParent' className='flex flex-col gap-4'>
-					<Column02
-						{...{
-							left: Rows([
-								<V01AHead {...data.attrs.head} />,
-								<V01AGoal {...data.attrs.goal} />,
-								<V01AExperience {...data.attrs.experience} />,
-								<V01AActivity {...data.attrs.activity} />,
-							]),
-							right: (
-								<div className='bg-blue-50'>
-									{Rows([
-										<V01AInfo {...data.attrs.head} />,
-										<V01AEducation {...data.attrs.education} />,
-										<V01ASkill {...data.attrs.skill} />,
-										<V01ACertificate {...data.attrs.certificate} />,
-									])}
-								</div>
-							),
-						}}
-					/>
-				</div>
+		<div className='w-full p-4 bg-white'>
+			<div id='sectionParent' className='flex flex-col gap-4'>
+				<Column02
+					{...{
+						left: Rows([
+							<V01AHead {...data.attrs.head} />,
+							<V01AGoal {...data.attrs.goal} />,
+							<V01AExperience {...data.attrs.experience} />,
+							<V01AActivity {...data.attrs.activity} />,
+						]),
+						right: (
+							<div className='bg-blue-50'>
+								{Rows([
+									<V01AInfo {...data.attrs.head} />,
+									<V01AEducation {...data.attrs.education} />,
+									<V01ASkill {...data.attrs.skill} />,
+									<V01ACertificate {...data.attrs.certificate} />,
+								])}
+							</div>
+						),
+					}}
+				/>
 			</div>
 		</div>
 	);
